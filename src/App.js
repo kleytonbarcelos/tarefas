eabacb9d0e83a0ef79ecb7d2ada1987eb9b4eaa5
Kleyton Barcelos <kleytonbarcelos@gmail.com>
2021-12-07 12:45:24 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App(){
+  const [tasks, setTasks] = useState([])
+  async function handleSubmit() {
+    let task = document.getElementById('tarefa').value
+    if (task) {
+      setTasks([
+        ...tasks,
+        {
+          id: Date.now(),
+          name: task.trim(),
+        },
+      ]);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <form className="mt-3 row" onSubmit={e => e.preventDefault()}>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" name="tarefa" id="tarefa" placeholder="Tarefa" aria-label="Tarefa" aria-describedby="basic-addon2" />
+        <div className="input-group-append">
+          <button className="btn btn-primary" onClick={()=>handleSubmit()}>Adicionar tarefa</button>
+        </div>
+      </div>
+    </form>
+      <div className="mt-3">
+        <ul className="list-group">
+          {tasks.map((task, index)=>(<li key={index} title={index} className="list-group-item" aria-current="true">{task.name}</li>))}
+        </ul>
+      </div>
     </div>
   );
 }
