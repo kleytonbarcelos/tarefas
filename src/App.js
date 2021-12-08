87f67815408679c64af0b1b20c594ceed47b09e4
Kleyton Barcelos <kleytonbarcelos@gmail.com>
2021-12-08 09:54:24 -0300

    add to deploy

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Modal, Button } from "react-bootstrap";
 
 function App(){
   const [tasks, setTasks] = useState([])
-  async function handleSubmit() {
-    let task = document.getElementById('novaTarefa').value
-    if (task) {
+
+  useEffect(() => {
+    let response = localStorage.getItem("tasks");
+    if(response){
+      response = JSON.parse(response)
+      response.sort((a, b) => (a.id > b.id) ? 1 : -1)
+      setTasks(response);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
+
+  function saveNewTask() {
+    if (newTarefa) {
       setTasks([
         ...tasks,
         {
           id: Date.now(),
-          name: task.trim(),
+          name: newTarefa.trim(),
         },
       ]);
+      setNewTarefa("")
     }
   }
-  useEffect(() => {
-    let response = localStorage.getItem("tasks");
-    let tasks = (JSON.parse(response)) || [];
-    setTasks(tasks);
-    //setTask("");
-  }, []);
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-  async function editTask(id) {
-    document.getElementById('tarefa').value = id
-    //setTasks(newtasks);
-    //let newSelecteds = selecteds.filter((item) => item != id);
-    //setSelecteds(newSelecteds);
+  function editTask(id) {
+    handleShow()
+    let taskSelected = tasks.filter((item) => parseInt(item.id) === parseInt(id))[0];
+    setTarefaId(taskSelected.id)
+    setTarefaNome(taskSelected.name)
   }
-  async function removeTask(id) {
-    let newtasks = tasks.filter((item) => item.id !== id);
+  function editTaskSave() {
+    if (tarefaNome) {
+      let allTasks = tasks.filter((item) => parseInt(item.id) !== parseInt(tarefaId));
+      allTasks.push({
+        id: parseInt(tarefaId),
+        name: tarefaNome.trim(),
+      })
+      allTasks.sort((a, b) => (a.id > b.id) ? 1 : -1)
+      setTasks(allTasks);
+      handleClose()
+    }
+  }
+  function removeTask(id) {
+    let newtasks = tasks.filter((item) => parseInt(item.id) !== parseInt(id));
     setTasks(newtasks);
     //let newSelecteds = selecteds.filter((item) => item != id);
     //setSelecteds(newSelecteds);
   }
-  // async function removeMultipleTasks() {
+  // function removeMultipleTasks() {
   //   let newtasks = tasks.filter((item) => {
   //     if (!selecteds.includes(item.id)) {
   //       return item;
@@ -55,50 +72,97 @@ function App(){
   //   localStorage.clear();
   //   setTasks([]);
   // }
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  const [newTarefa, setNewTarefa] = useState();
+  const [tarefaId, setTarefaId] = useState();
+  const [tarefaNome, setTarefaNome] = useState();
+
   return (
     <div className="container">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        centered
+        >
+        <Modal.Header closeButton>
+          <Modal.Title>Editar tarefa</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type="hidden" className="form-control" value={tarefaId} />
+          <div className="mb-3">
+            <label for="tarefa" className="form-label">Tarefa</label>
+            <input
+              type="text"
+              className="form-control"
+              value={tarefaNome} 
+              onChange={e => setTarefaNome(e.target.value)}
+              autoFocus={true}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  editTaskSave()
+                }
+              }}
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fechar
+          </Button>
+          <Button variant="primary" onClick={()=>editTaskSave()}>Salvar</Button>
+        </Modal.Footer>
+      </Modal>
       <form className="mt-3 row" onSubmit={e => e.preventDefault()}>
         <div className="input-group mb-3">
-          <input type="text" className="form-control" name="novaTarefa" id="novaTarefa" placeholder="Tarefa" aria-label="Tarefa" aria-describedby="basic-addon2" />
+          <input type="text" className="form-control" value={newTarefa} onChange={e => setNewTarefa(e.target.value)} placeholder="Tarefa" aria-label="Tarefa" aria-describedby="basic-addon2" />
           <div className="input-group-append">
-            <button className="btn btn-primary" onClick={()=>handleSubmit()}>Adicionar tarefa</button>
+            <button className="btn btn-primary" onClick={()=>saveNewTask()}>Adicionar tarefa</button>
           </div>
         </div>
       </form>
-      <div>{JSON.stringify(tasks)}</div>
+      {/* <div>{JSON.stringify(tasks)}</div> */}
       <div className="mt-3">
         <ul className="list-group">
           {tasks.map((task, index)=>(
             <li key={index} title={index} className="d-flex justify-content-between list-group-item" aria-current="true">
               <div>{task.name}</div>
               <div>
-                <button class="me-2 btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>editTask(task.id)}>Editar</button>
-                <button class="btn btn-danger" onClick={()=>removeTask(task.id)}>Excluir</button>
+                <span className="badge rounded-pill bg-secondary hand me-2" onClick={()=>editTask(task.id)}>Editar</span>
+                <span className="badge rounded-pill bg-danger hand" onClick={()=>removeTask(task.id)}>Excluir</span>
+                {/* <span className="me-2 badge bg-secondary hand" onClick={()=>editTask(task.id)}><i class="fa fa-edit" aria-hidden="true"></i> Editar</span>
+                <span className="badge bg-danger hand" onClick={()=>removeTask(task.id)}>Excluir</span> */}
               </div>
             </li>
           ))}
         </ul>
       </div>
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Editar tarefa</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      {/* <div className="modal fade" show={show} onHide={handleClose} ref={modalRef} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Editar tarefa</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-              <div class="mb-3">
-                <label for="tarefa" class="form-label">Tarefa</label>
-                <input type="text" class="form-control" id="tarefa" placeholder="Tarefa" />
+            <div className="modal-body">
+              <input type="hidden" className="form-control" id="tarefaId" />
+              <div className="mb-3">
+                <label for="tarefa" className="form-label">Tarefa</label>
+                <input type="text" className="form-control" id="tarefa" placeholder="Tarefa" />
               </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-              <button type="button" class="btn btn-primary">Salvar</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+              <button type="button" className="btn btn-primary" onClick={()=>editTaskSave()}>Salvar</button>
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
